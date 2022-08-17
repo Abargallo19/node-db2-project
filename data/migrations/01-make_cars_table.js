3cc92f9e3625f4f27cc433981e6dc66e04cfd739
Allesandria <mufasabugatti@Mufasas-MBP.lan>
2022-08-16 21:55:58 -0400

    knex down function built

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,14 @@
 exports.up = function (knex) {
   // DO YOUR MAGIC
+return knex.schema.createTable('cars', tbl => {
+  tbl.increments();
+  tbl.text()
+})
+
+
 };
 
 exports.down = function (knex) {
   // DO YOUR MAGIC
+  return knex.schema.dropTableIfExists('cars');
 };
